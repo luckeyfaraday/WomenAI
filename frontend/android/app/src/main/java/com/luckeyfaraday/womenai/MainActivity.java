7539f084327f958910ae797853bf3e485229a43c
luckeyfaraday <soysatoshi@protonmail.com>
2026-01-25 03:02:46 +0100

    fix(android): Enable Third-Party Cookies in WebView

--- frontend/android/app/src/main/java/com/luckeyfaraday/womenai/MainActivity.java
+++ frontend/android/app/src/main/java/com/luckeyfaraday/womenai/MainActivity.java
@@ -1,5 +1,16 @@
 package com.luckeyfaraday.womenai;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            android.webkit.CookieManager.getInstance().setAcceptThirdPartyCookies(this.getBridge().getWebView(), true);
+        }
+    }
+}
